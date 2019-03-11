c04e4b7eed83d67b6ed8575401ef250383dcc36f
Peter Park <parkdhp@gmail.com>
2019-03-11 19:15:45 -0400

    feat(mutations and queries): added resolvers to fetch add and fetch items

--- server/src/resolvers/Mutation.js
+++ server/src/resolvers/Mutation.js
@@ -1,3 +1,17 @@
-const Mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    // TODO: Check if they are logged in
+    // access db through server context
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args,
+        },
+      },
+      info
+    );
+    return item;
+  },
+};
 
 module.exports = Mutations;
