0488b51e50b693fd3af3ff374310878c3a6fc196
Peter Park <parkdhp@gmail.com>
2019-03-12 04:08:18 -0400

    feat(CreateItem.js): added image file upload field, image preview, and functionality

--- client/components/CreateItem.js
+++ client/components/CreateItem.js
@@ -40,6 +40,26 @@ class CreateItem extends Component {
     this.setState({ [name]: val });
   };
 
+  uploadFile = async e => {
+    const files = e.target.files;
+    const data = new FormData();
+    data.append('file', files[0]);
+    data.append('upload_preset', 'vndrrr');
+    const res = await fetch(
+      'https://api.cloudinary.com/v1_1/parkdhp/image/upload',
+      {
+        method: 'POST',
+        body: data,
+      }
+    );
+    const file = await res.json();
+    console.log(file);
+    this.setState({
+      image: file.secure_url,
+      largeImage: file.eager[0].secure_url,
+    });
+  };
+
   render() {
     const { title, description, image, largeImage, price } = this.state;
     return (
@@ -60,6 +80,19 @@ class CreateItem extends Component {
           >
             <Error error={error} />
             <fieldset disabled={loading} aria-busy={loading}>
+              <label htmlFor="file">
+                Image
+                <input
+                  type="file"
+                  id="file"
+                  name="file"
+                  placeholder="Upload an image"
+                  required
+                  onChange={this.uploadFile}
+                />
+                {image && <img width="200" src={image} alt="Upload Preview" />}
+              </label>
+
               <label htmlFor="title">
                 Title
                 <input
