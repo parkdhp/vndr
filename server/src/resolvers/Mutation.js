e92497cf81a63f04988b4ee7c76f7c568c5ef630
Peter Park <parkdhp@gmail.com>
2019-03-12 04:33:48 -0400

    feat: added backend logic for handling item updates

--- server/src/resolvers/Mutation.js
+++ server/src/resolvers/Mutation.js
@@ -12,6 +12,23 @@ const Mutations = {
     );
     return item;
   },
+  updateItem(parent, args, ctx, info) {
+    // first take a copy of the updates
+    const updates = { ...args };
+    // remove the ID from the updates because this will give us the id that gets passed in and and we will not update the id
+    delete updates.id;
+    // run the update method, since returning promise based function will wait for update to pass
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: {
+          id: args.id,
+        },
+      },
+      // how this update function knows what to return. This will contain the query that we send it from the client side to return the item
+      info
+    );
+  },
 };
 
 module.exports = Mutations;
