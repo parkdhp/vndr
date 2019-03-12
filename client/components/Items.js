e99c3fedea019ab71b30a04261e617e9a571b43c
Peter Park <parkdhp@gmail.com>
2019-03-11 20:06:24 -0400

    feat(Items.js): added styles to center ItemsList and display title

--- client/components/Items.js
+++ client/components/Items.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
+import styled from 'styled-components';
 
 const ALL_ITEMS_QUERY = gql`
   query ALL_ITEMS_QUERY {
@@ -15,20 +16,37 @@ const ALL_ITEMS_QUERY = gql`
   }
 `;
 
+const Center = styled.div`
+  text-align: center;
+`;
+
+const ItemsList = styled.div`
+  display: grid;
+  grid-auto-columns: 1fr 1fr;
+  grid-gap: 60px;
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+`;
+
 class Items extends Component {
   render() {
     return (
-      <div>
+      <Center>
         <p>Items</p>
         <Query query={ALL_ITEMS_QUERY}>
           {({ data, error, loading }) => {
-            console.log(data);
             if (loading) return <p>Loading...</p>;
             if (error) return <p>Error: {error.message}</p>;
-            return <p>Found: {data.items.length}</p>;
+            return (
+              <ItemsList>
+                {data.items.map(item => (
+                  <p>{item.title}</p>
+                ))}
+              </ItemsList>
+            );
           }}
         </Query>
-      </div>
+      </Center>
     );
   }
 }
