a7e86a3162f45ceb69e1a1879a2fa3d510aa7624
Peter Park <parkdhp@gmail.com>
2019-03-13 00:50:10 -0400

    feat(index.js): added middleware to verify jwt token and add userId to cross reference db

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,22 @@
 const cookieParser = require('cookie-parser');
 require('dotenv').config({ path: 'variables.env' });
+const jwt = require('jsonwebtoken');
 const createServer = require('./createServer');
 const db = require('./db');
 
 const server = createServer();
 
 server.express.use(cookieParser());
-// TODO Use express middleware to populate current user
+// decode the JWT so we can get the user ID on each request
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    // put userID onto the req for future requests to access
+    req.userId = userId;
+  }
+  next();
+});
 
 server.start(
   {
