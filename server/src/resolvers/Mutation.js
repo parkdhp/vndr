81530c5b7d74755684ddda7ea2e367e80a9ea9f1
Peter Park <parkdhp@gmail.com>
2019-03-14 16:36:29 -0400

    feat(Mutation.js): added addToCart mutation to add items to cart and connect items to user

--- server/src/resolvers/Mutation.js
+++ server/src/resolvers/Mutation.js
@@ -212,6 +212,46 @@ const Mutations = {
       info
     );
   },
+  async addToCart(parent, args, ctx, info) {
+    // make sure user is signed in
+    const { userId } = ctx.request;
+    if (!userId) {
+      throw new Error('You must be logged in!');
+    }
+    // query the users current cart
+    const [existingCartItem] = await ctx.db.query.cartItems({
+      where: {
+        user: { id: userId },
+        item: { id: args.id },
+      },
+    });
+    // check if item is already in the cart and increment by 1 if it is
+    if (existingCartItem) {
+      return ctx.db.mutation.updateCartItem(
+        {
+          where: { id: existingCartItem.id },
+          data: { quantity: existingCartItem.quantity + 1 },
+        },
+        info
+      );
+    }
+    // if not, create a fresh CartItem for the user
+    return ctx.db.mutation.createCartItem(
+      {
+        data: {
+          user: {
+            // connect to user id
+            connect: { id: userId },
+          },
+          item: {
+            // connect to item id
+            connect: { id: args.id },
+          },
+        },
+      },
+      info
+    );
+  },
 };
 
 module.exports = Mutations;
