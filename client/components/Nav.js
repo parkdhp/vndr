3f36ef45248186db2e13640cd79e465ae42bf38d
Peter Park <parkdhp@gmail.com>
2019-03-18 22:40:45 -0400

    fix(Nav.js):

--- client/components/Nav.js
+++ client/components/Nav.js
@@ -21,9 +21,9 @@ const Nav = () => (
             <Link href="/orders">
               <a>Orders</a>
             </Link>
-            <Link href="/me">
+            {/* <Link href="/me">
               <a>Account</a>
-            </Link>
+            </Link> */}
             <Signout />
             <Mutation mutation={TOGGLE_CART_MUTATION}>
               {toggleCart => (
